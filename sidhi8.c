16b9fd2eb5822bdd268968e1703f586bbf925689
Sidhi Rani <sidhirani108@gmail.com>
2026-08-29 00:21:21 +0530

    Add files via upload

---
+++ sidhi8.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, temp;
+
+    printf("\n Enter First Number:");
+    scanf("%d", &a);
+
+    printf("\n Enter First Number:");
+    scanf("%d", &b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("\n After Swapping:\n");
+    printf("\n First Number = %d\n", a);
+    printf("\n Second Number = %d\n", b);
+
+    return 0;
+
+}
---
+++ sidhi9.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    // This is a Single - line comment....
+
+    /*
+    This is a Multi - line comment.
+    It can span across multiple lines.
+    */
+
+    return 0;
+}
